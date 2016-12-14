4baa284a82cf4cad70c071cbc48b952b5a73fc1f
Keir Smith <kaffse@gmail.com>
2016-12-14 17:36:28 +0000

    Reworked player prefab
    + Added player head prefab to track postition across the network
    * Player prefab now playerrig and is there's only one per instance

---
+++ Assets/TankCrewVR/Scripts/PlayerHeadController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHeadController : MonoBehaviour {
+	private GameObject playerHeadCam;
+
+	//TODO Move this crap
+	private GameObject spawn;
+	private GameObject playerRig;
+
+	// Use this for initialization
+	void Start () {
+		playerHeadCam = GameObject.Find("Camera (eye)");
+		playerRig = GameObject.Find("PlayerRig");
+
+		GameObject[] spawns = GameObject.FindGameObjectsWithTag("Respawn");
+		spawn = spawns[0];
+		spawn.transform.tag = "Untagged";
+
+		playerRig.transform.SetParent(spawn.transform);
+		playerRig.transform.localPosition = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = playerHeadCam.transform.position;
+	}
+}
